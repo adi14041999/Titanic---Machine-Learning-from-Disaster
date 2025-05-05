1585465b59a1b7d122e15e27251ae1f5a128b5a8
Aditya Prabhu <adigithub@gmail.com>
2025-05-05 00:14:57 -0700

    Implemented a voting based classifier

--- script.py
+++ script.py
@@ -1,73 +1,132 @@
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_c
-from sklearn.svm import LinearSVC
-import matplotlib.pyplot as plt
+import numpy as np 
+import pandas as pd
+from sklearn.model_selection import cross_val_score
 from sklearn.naive_bayes import GaussianNB
+from sklearn.linear_model import LogisticRegression
+from sklearn import tree
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
 
-train_data = pd.read_csv("train.csv")
-train_data.head()
+training = pd.read_csv('train.csv')
+test = pd.read_csv('test.csv')
 
-test_data = pd.read_csv("test.csv")
-test_data.head()
+training['train_test'] = 1
+test['train_test'] = 0
+test['Survived'] = np.nan
+# Combine training and test data to do the same transformations on both sets
+all_data = pd.concat([training,test])
 
-# See how many survived
-women = train_data.loc[train_data.Sex == 'female']["Survived"]
-rate_women = sum(women)/len(women)
-print("% of women who survived:", rate_women)
+# Create all categorical variables that we did above for both training and test sets 
+all_data['cabin_multiple'] = all_data.Cabin.apply(lambda x: 0 if pd.isna(x) else len(x.split(' ')))
+all_data['cabin_adv'] = all_data.Cabin.apply(lambda x: str(x)[0])
+all_data['numeric_ticket'] = all_data.Ticket.apply(lambda x: 1 if x.isnumeric() else 0)
+all_data['ticket_letters'] = all_data.Ticket.apply(lambda x: ''.join(x.split(' ')[:-1]).replace('.','').replace('/','').lower() if len(x.split(' ')[:-1]) >0 else 0)
+all_data['name_title'] = all_data.Name.apply(lambda x: x.split(',')[1].split('.')[0].strip())
 
-men = train_data.loc[train_data.Sex == 'male']["Survived"]
-rate_men = sum(men)/len(men)
-print("% of men who survived:", rate_men)
+# Impute nulls for continuous data 
+#all_data.Age = all_data.Age.fillna(training.Age.mean())
+all_data.Age = all_data.Age.fillna(training.Age.median())
+#all_data.Fare = all_data.Fare.fillna(training.Fare.mean())
+all_data.Fare = all_data.Fare.fillna(training.Fare.median())
 
+# Drop null 'embarked' rows. Only 2 instances of this in training and 0 in test 
+all_data.dropna(subset=['Embarked'],inplace = True)
 
-y = train_data["Survived"]
+# Tried log norm of sibsp (not used)
+all_data['norm_sibsp'] = np.log(all_data.SibSp+1)
 
-nan_count_Pclass = train_data['Pclass'].isna().sum()
-nan_count_Sex = train_data["Sex"].isna().sum()
-nan_count_Age = train_data["Age"].isna().sum()
-nan_count_SibSp = train_data["SibSp"].isna().sum()
-nan_count_Parch = train_data["Parch"].isna().sum()
-nan_count_Fare = train_data["Fare"].isna().sum()
-nan_count_Embarked = train_data["Embarked"].isna().sum()
-print("nan count Pclass:", nan_count_Pclass)
-print("nan count Sex:", nan_count_Sex)
-print("nan count Age:", nan_count_Age)
-print("% of Age NaN:", nan_count_Age / len(train_data))
-print("nan count SibSp:", nan_count_SibSp)
-print("nan count Parch:", nan_count_Parch)
-print("nan count Fare:", nan_count_Fare)
-print("nan count Embarked:", nan_count_Embarked)
+# log norm of fare (used)
+all_data['norm_fare'] = np.log(all_data.Fare+1)
 
-nan_count_Pclass_test = test_data['Pclass'].isna().sum()
-nan_count_Sex_test = test_data["Sex"].isna().sum()
-nan_count_Age_test = test_data["Age"].isna().sum()
-nan_count_SibSp_test = test_data["SibSp"].isna().sum()
-nan_count_Parch_test = test_data["Parch"].isna().sum()
-nan_count_Fare_test = test_data["Fare"].isna().sum()
-nan_count_Embarked_test = test_data["Embarked"].isna().sum()
-print("nan count Pclass test:", nan_count_Pclass_test)
-print("nan count Sex test:", nan_count_Sex_test)
-print("nan count Age test:", nan_count_Age_test)
-print("% of Age NaN test:", nan_count_Age_test / len(test_data))
-print("nan count SibSp test:", nan_count_SibSp_test)
-print("nan count Parch test:", nan_count_Parch_test)
-print("nan count Fare test:", nan_count_Fare_test)
-print("nan count Embarked test:", nan_count_Embarked_test)
+# Explicitly convert Pclass into str type
+all_data.Pclass = all_data.Pclass.astype(str)
 
+# Created dummy variables from categories (OneHotEncoder)
+all_dummies = pd.get_dummies(all_data[['Pclass','Sex','Age','SibSp','Parch','norm_fare','Embarked','cabin_adv','cabin_multiple','numeric_ticket','name_title','train_test']])
 
-features = ["Pclass", "Sex", "SibSp", "Parch", "Fare", "Embarked"]
+# Peek at first 5 items of the dataset
+print(all_dummies.head(5))
 
+# Split to train test again
+X_train = all_dummies[all_dummies.train_test == 1].drop(['train_test'], axis =1)
+X_test = all_dummies[all_dummies.train_test == 0].drop(['train_test'], axis =1)
 
-train_data["Embarked"].fillna(train_data["Embarked"].mode().iloc[0], inplace=True)
-test_data["Fare"].fillna(test_data["Fare"].mean(), inplace=True)
+y_train = all_data[all_data.train_test==1].Survived
 
-X = pd.get_dummies(train_data[features])
-X_test = pd.get_dummies(test_data[features])
-model = GaussianNB()
-#model = LinearSVC(random_state=0)
-model.fit(X, y)
-predictions = model.predict(X_test)
+# Scale data 
+"""
+fit() computes the mean and stdev to be used for later scaling, note it's just a computation with no scaling done.
 
-output = pd.DataFrame({'PassengerId': test_data.PassengerId, 'Survived': predictions})
-output.to_csv('submission.csv', index=False)
-print("Your submission was successfully saved!")
+transform() uses the previously computed mean and stdev to scale the data 
+(subtract mean from all values and then divide it by stdev).
+
+fit_transform() does both at the same time. So you can do it with just 1 line of code.
+"""
+from sklearn.preprocessing import StandardScaler
+scale = StandardScaler()
+all_dummies_scaled = all_dummies.copy()
+all_dummies_scaled[['Age','SibSp','Parch','norm_fare']]= scale.fit_transform(all_dummies_scaled[['Age','SibSp','Parch','norm_fare']])
+
+# Peek at first 5 items of the transformed dataset
+print(all_dummies_scaled.head(5))
+
+X_train_scaled = all_dummies_scaled[all_dummies_scaled.train_test == 1].drop(['train_test'], axis =1)
+X_test_scaled = all_dummies_scaled[all_dummies_scaled.train_test == 0].drop(['train_test'], axis =1)
+
+y_train = all_data[all_data.train_test==1].Survived
+
+gnb = GaussianNB()
+cv = cross_val_score(gnb,X_train_scaled,y_train,cv=5)
+print(cv)
+print(cv.mean())
+
+lr = LogisticRegression(max_iter = 2000)
+cv = cross_val_score(lr,X_train_scaled,y_train,cv=5)
+print(cv)
+print(cv.mean())
+
+dt = tree.DecisionTreeClassifier(random_state = 1)
+cv = cross_val_score(dt,X_train_scaled,y_train,cv=5)
+print(cv)
+print(cv.mean())
+
+knn = KNeighborsClassifier()
+cv = cross_val_score(knn,X_train_scaled,y_train,cv=5)
+print(cv)
+print(cv.mean())
+
+rf = RandomForestClassifier(random_state = 1)
+cv = cross_val_score(rf,X_train_scaled,y_train,cv=5)
+print(cv)
+print(cv.mean())
+
+svc = SVC(probability = True)
+cv = cross_val_score(svc,X_train_scaled,y_train,cv=5)
+print(cv)
+print(cv.mean())
+
+from xgboost import XGBClassifier
+xgb = XGBClassifier(random_state =1)
+cv = cross_val_score(xgb,X_train_scaled,y_train,cv=5)
+print(cv)
+print(cv.mean())
+
+"""
+A Voting Classifier takes all of the inputs and averages the results. 
+For a "hard" voting classifier each classifier gets 1 vote "yes" or "no" and the result is just a popular vote. 
+For this, you generally want odd numbers. 
+A "soft" classifier averages the confidence of each of the models. 
+If the average confidence is > 50% that it is a 1 it will be counted as such.
+"""
+from sklearn.ensemble import VotingClassifier
+voting_clf = VotingClassifier(estimators = [('lr',lr),('knn',knn),('rf',rf),('gnb',gnb),('svc',svc),('xgb',xgb)], voting = 'soft') 
+cv = cross_val_score(voting_clf,X_train_scaled,y_train,cv=5)
+print(cv)
+print(cv.mean())
+
+voting_clf.fit(X_train_scaled,y_train) # train
+y_hat_base_vc = voting_clf.predict(X_test_scaled).astype(int) # test
+basic_submission = {'PassengerId': test.PassengerId, 'Survived': y_hat_base_vc}
+base_submission = pd.DataFrame(data=basic_submission)
+base_submission.to_csv('submission.csv', index=False)
